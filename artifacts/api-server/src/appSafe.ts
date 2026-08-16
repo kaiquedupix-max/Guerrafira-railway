7cc8ec9371e74faf403e3e759c40b1f732c65167
kaiquedupix-max <kaiquedupix@gmail.com>
2026-08-16 16:17:12 -0300

    Finish protected vote upload interface

--- artifacts/api-server/src/appSafe.ts
+++ artifacts/api-server/src/appSafe.ts
@@ -32,7 +32,7 @@ app.use(
 );
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "18mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
